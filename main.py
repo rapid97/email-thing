8ca2d6078bbeeb4a7199a3ce2ace49728de2caa7
rapid97 <134788044+rapid97@users.noreply.github.com>
2025-09-28 15:20:26 +0530

    broke some operations into functions
    server setup, authorisation, folder select, folder search have their own functions along with automatic timer objects

---
+++ main.py
@@ -0,0 +1,153 @@
+import threading
+import time
+import imaplib, email
+from timeperf import TimePerf
+
+#yahoo: imap.mail.yahoo.com
+#gmail: imap.gmail.com
+mails = [("email id", "app password", "mail imap server", "number of mails"),
+         ("email id", "app password", "mail imap server", "number of mails"),
+         ("email id", "app password", "mail imap server", "number of mails")]
+
+def time_perf(func):
+    def wrapper():
+        time_start = time.perf_counter()
+        func()
+        elapsed = time.perf_counter() - time_start
+        print (f"{func} took {elapsed} seconds")
+
+    return wrapper
+
+def setup_SSL_server(server):
+    server_timer = TimePerf(name = "server_setup", auto = True)
+    return imaplib.IMAP4_SSL(server)
+
+def authorise_creds(mail, creds):
+    cred_timer = TimePerf(name = f"{creds[0]} authorisation", auto = True)
+    mail.login(creds[0], creds[1])
+
+def select_folder(mail, folder):
+    folder_select_timer = TimePerf(name = "folder selection", auto = True)
+    mail.select(folder)
+
+def search_folder(mail, search_criteria = "ALL"):
+    mail_searcher_timer = TimePerf(name = "mail searcher", auto = True)
+    return mail.search(None, search_criteria)
+
+def data_to_list(data):
+    split_timer = TimePerf(name = "list splitter", auto = True)
+    return data[0].split()
+
+#returns the number of threads, no. of files for each thread and correction factor
+#correction factor exists to get the correct number of files for even number of threads
+def thread_groups(threads, num):
+    return threads, int(num), int((num%1 * threads))
+
+#gets the amount of threads to execute for file fetching
+def get_thread_amount(num):
+    n = 0
+    while (num > 10):                                               #tries to make enough threads to keep time consumed around 10 seconds
+        num = num/2
+        n += 1
+
+    return thread_groups(2**n, num)
+
+#sets up imap server and fetches emails according to the executing thread
+def fetch_group(start, finish, mail_store, creds, correction, num, thread = None):
+    store = []                                                      #stores fetched emails locally before merging with main storage
+    mail = setup_SSL_server(creds[2])                               #sets up imap server with host specified in creds[2]
+    authorise_creds(mail, creds)                                    #logs into the server
+    select_folder(mail, "INBOX")                                    #selects the folder to look for mails in
+    result, data = search_folder(mail, "ALL")                       #searches for specific mail in selected folder
+    mail_ids = data[0].split()                                      #gets individual mail ids in a list
+ 
+    for i in range(start, finish, -1):                              #start and finish of mail ids changes according to executing thread
+        result, msg_data = mail.fetch(mail_ids[i], "(RFC822)")      #get the mail data from given mail id
+        #print (f"fetched mail for {creds[0]} with {thread}")
+        msg = email.message_from_bytes(msg_data[0][1])              #convert to message from bytes
+        store.append(msg["subject"])
+
+    #print (finish, "+++", -num + correction)
+    if finish == -num + (correction - 1):                           #if last thread for this operation is executing
+        for i in range(finish, finish - correction, -1):            #get the last few mails according to correction factor
+            result, msg_data = mail.fetch(mail_ids[i], "(RFC822)")  
+            #print (f"fetched mail for {creds[0]} with {thread}")
+            msg = email.message_from_bytes(msg_data[0][1])
+            store.append(msg["subject"])
+        
+    mail.logout()                                                   #logout of server
+    
+    if thread != None:                                              #if executing thread is the first one then merge into main storage
+        thread.join()                                               #else wait for the previous thread to finish
+                                                                    #before merging mails into main storage
+    mail_store += store
+    
+#sets up all the threads required to fetch mails in groups
+def get_mail(creds, num, thread = None):
+    #get_mail_timer = TimePerf(name = "get_mail_func")
+    #get_mail_timer.startTick()
+    thread_amount, file_group, correction = get_thread_amount(num)  #gets required data to create the threads
+    thread_list = {}                                                #stores created threads
+    mail_store = []                                                 #main storage for fetched mails
+
+    #print ("thread:", thread_amount)
+    for n in range(1, thread_amount + 1):
+        #print (-(file_group*(n-1))-1, -(file_group*n)-1)
+        if n == 1:
+            prev = None                                             #if first thread, then no need to wait for any previous threads
+        else:
+            prev = thread_list[n-1]                                 #subsequent threads wait for previous threads
+
+        """
+            args 1: -(file_group*(n-1))-1, gets the first mail id to fetch in the loop for this nth thread (starts with -1 for example to get latest mail)
+            args 2: -(file_group*n)-1, gets the last mail id to fetch upto in the loop for this nth thread
+        """
+        thread_list[n] = threading.Thread(target = fetch_group, args = (-(file_group*(n-1))-1, -(file_group*n)-1, mail_store, creds, correction, num, prev))
+
+    
+    for t in thread_list:
+        thread_list[t].start()                                      #starts all threads for fetch_group
+
+    for t in thread_list:
+        thread_list[t].join()                                       #waits for all threads to finish before moving on to printing mails
+                                          
+    #fetch_group(-1, -(num+1), mail_store, creds)
+
+    if thread != None:                                              #all threads except the first will wait 
+        thread.join()                                               #for previous threads to finish before printing
+        
+    print(f"Latest {num} emails for account: {creds[0]}: ")         #print out all mail in main storage 
+    for i, m in enumerate(mail_store):                              #after all threads have finished executing
+        print (i, m)
+        
+    #get_mail_timer.endTick()
+    print ()
+        
+    #return mail_store
+
+def main():
+    main_timer = TimePerf(name = "Main")
+    email_threads = {}
+    #creating threads for each email id, performant mail limit is like 100 for each email, gmail is messing up kinda
+    for n, creds in enumerate(mails):
+        #print (-(file_group*(n-1))-1, -(file_group*n)-1)
+        if n == 0:
+            prev = None                                             #if first thread, then no need to wait for any previous threads
+        else:
+            prev = email_threads[n-1]                               #subsequent threads wait for previous threads
+
+        email_threads[n] = threading.Thread(target = get_mail, args = (creds, creds[3], prev))
+
+    for t in email_threads:
+        email_threads[t].start()
+
+    for t in email_threads:
+        email_threads[t].join()
+
+    #show_mail(mails[0], 15)
+
+
+if __name__ == "__main__":
+    main()
+
+
